81f0d223a152c83f485858466c6d732a2b4952aa
Mahfazzalin <mahfazzalin1@gmail.com>
2024-06-28 02:24:15 +0600

    update

--- static/js/game.js
+++ static/js/game.js
@@ -15,6 +15,7 @@ socket.on('room_created', (data) => {
     alert(`Room created with key: ${data.room_key}`);
     document.getElementById('join_game').classList.add('hidden');
     document.getElementById('game_dashboard').classList.remove('hidden');
+    socket.emit('start_game', { room_key: data.room_key });
 });
 
 socket.on('joined_room', (data) => {
@@ -25,6 +26,32 @@ socket.on('joined_room', (data) => {
 
 socket.on('start_game', () => {
     alert('Game is starting!');
+});
+
+socket.on('cards_assigned', (data) => {
+    alert(`Cards assigned: ${JSON.stringify(data)}`);
+    for (let player in data) {
+        if (data[player] === "900=King") {
+            alert(`${player} is the King`);
+        }
+    }
+});
+
+socket.on('identify_king', (data) => {
+    if (data.king === document.getElementById('username').value) {
+        const target = prompt("Who will Police catch? (Enter player name)");
+        socket.emit('king_decision', { room_key: document.getElementById('room_key').value, target: target });
+    }
+});
+
+socket.on('round_result', (data) => {
+    alert(`Police caught ${data.target}. Points: ${JSON.stringify(data.points)}`);
+});
+
+socket.on('game_over', (data) => {
+    alert(`Game over! Winner: ${data.winner}. Points: ${JSON.stringify(data.points)}`);
+    document.getElementById('game_dashboard').classList.add('hidden');
+    document.getElementById('game_over').classList.remove('hidden');
 });
 
 socket.on('error', (data) => {
